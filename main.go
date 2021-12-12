369a469097f434db31bcbb65d5ab5da87426b54d
amirrezaask <raskarpour@gmail.com>
2021-12-13 00:35:41 +0330

    better flush function

--- main.go
+++ main.go
@@ -7,7 +7,6 @@ import (
 
 const (
 	tokenType_Symbol         = "symbol"
-	tokenType_String         = "string"
 	tokenType_Number         = "number"
 	tokenType_Colon          = "colon"
 	tokenType_Asterix        = "asterix"
@@ -28,81 +27,59 @@ func (t token) String() string {
 	return fmt.Sprintf("<%s %s>", t.typ, t.value)
 }
 
-func flush(tokens *[]token, buffType string, buff string) {
-	if buff != "" {
-		if buffType == "" {
-			buffType = "symbol"
+func flush(tokens *[]token, buffType *string, buff *string) {
+	if *buff != "" {
+		if *buffType == "" {
+			*buffType = "symbol"
 		}
-		*tokens = append(*tokens, token{typ: buffType, value: buff})
+		*tokens = append(*tokens, token{typ: *buffType, value: *buff})
 	}
+	*buff = ""
+	*buffType = ""
 
 }
-
 func lex(code string) []token {
 	var tokens []token
 	var buffType string
 	var buff string
 	for _, char := range code {
 		if char == '[' {
-			flush(&tokens, buffType, buff)
-			buff = ""
-			buffType = ""
+			flush(&tokens, &buffType, &buff)
 			tokens = append(tokens, token{typ: tokenType_OpenSqBracket, value: "["})
 		} else if char == ']' {
-			flush(&tokens, buffType, buff)
-			buff = ""
-			buffType = ""
+			flush(&tokens, &buffType, &buff)
 			tokens = append(tokens, token{typ: tokenType_CloseSqBracket, value: "]"})
 		} else if char == '*' {
-			flush(&tokens, buffType, buff)
-			buff = ""
-			buffType = ""
+			flush(&tokens, &buffType, &buff)
 			tokens = append(tokens, token{typ: tokenType_Asterix, value: "*"})
 		} else if char == '+' {
-			flush(&tokens, buffType, buff)
-			buff = ""
-			buffType = ""
+			flush(&tokens, &buffType, &buff)
 			tokens = append(tokens, token{typ: tokenType_Plus, value: "+"})
 		} else if char == '-' {
-			flush(&tokens, buffType, buff)
-			buff = ""
-			buffType = ""
+			flush(&tokens, &buffType, &buff)
 			tokens = append(tokens, token{typ: tokenType_Minus, value: "-"})
 		} else if char == '/' {
-			flush(&tokens, buffType, buff)
-			buff = ""
-			buffType = ""
+			flush(&tokens, &buffType, &buff)
 			tokens = append(tokens, token{typ: tokenType_Div, value: "/"})
 		} else if char == '%' {
-			flush(&tokens, buffType, buff)
-			buff = ""
-			buffType = ""
+			flush(&tokens, &buffType, &buff)
 			tokens = append(tokens, token{typ: tokenType_Mod, value: "%"})
 		} else if unicode.IsDigit(char) {
 			buffType = tokenType_Number
 			buff = buff + string(char)
 		} else if unicode.IsLetter(char) {
 			buff = buff + string(char)
-		} else if char == '"' {
-			buffType = tokenType_String
 		} else if char == ':' {
-			flush(&tokens, buffType, buff)
-			buff = ""
-			buffType = ""
+			flush(&tokens, &buffType, &buff)
 			tokens = append(tokens, token{typ: tokenType_Colon, value: ":"})
 		} else if char == ' ' {
-			flush(&tokens, buffType, buff)
-			buff = ""
-			buffType = ""
+			flush(&tokens, &buffType, &buff)
 		}
 	}
-	if buff != "" {
-		tokens = append(tokens, token{typ: buffType, value: buff})
-	}
-	buff = ""
-	buffType = ""
+	flush(&tokens, &buffType, &buff)
 	return tokens
 }
+
 func main() {
 	code := `map [1 2 3] x: x * 2`
 	fmt.Printf("%+v\n", lex(code))
